5003463920a9e122b8c01e0a40a6634b169e6564
mimuogi <mimuogi@gmail.com>
2024-07-27 13:26:42 +0200

    routing changes

--- src/main.tsx
+++ src/main.tsx
@@ -11,57 +11,54 @@ import { ContactPage } from "./pages/contact.tsx";
 import { PodcastPage } from "./pages/podcast.tsx";
 import { Layout } from "./components/layout/layout";
 
-const router = createBrowserRouter(
-	[
-		{
-			path: "/",
-			element: <Layout />, // Use Layout here to ensure it wraps around the pages
-			children: [
-				{
-					path: "/",
-					element: <Home />,
-				},
-				{
-					path: "/nosaltres",
-					element: <About />,
-				},
-				{
-					path: "/noticies",
-					element: <News />,
-				},
-				{
-					path: "/podcast",
-					element: <PodcastPage />,
-				},
-				{
-					path: "/contacte",
-					element: <ContactPage />,
-				},
-				{
-					path: "/post/:id",
-					element: <PostPage />,
-					loader: ({ params }) => {
-						const post = postlist.find((post) => post.id === params.id);
-						if (!post) {
-							throw new Error("Post not found");
-						}
-						return { post };
-					},
-				},
-				{
-					path: "*",
-					element: <Home />,
-				},
-			],
-		},
-	],
+const router = createBrowserRouter([
 	{
-		basename: "/casa-nostra-web",
-	}
-);
+		path: "/",
+		element: <Layout />, // Use Layout here to ensure it wraps around the pages
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/nosaltres",
+				element: <About />,
+			},
+			{
+				path: "/noticies",
+				element: <News />,
+			},
+			{
+				path: "/podcast",
+				element: <PodcastPage />,
+			},
+			{
+				path: "/contacte",
+				element: <ContactPage />,
+			},
+			{
+				path: "/post/:id",
+				element: <PostPage />,
+				loader: ({ params }) => {
+					const post = postlist.find((post) => post.id === params.id);
+					if (!post) {
+						throw new Error("Post not found");
+					}
+					return { post };
+				},
+			},
+			{
+				path: "*",
+				element: <Home />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
 	</React.StrictMode>
 );
+
+//{basename: "/casa-nostra-web",}
