e3bd311c6e20ab14f92d9e41c4e26dd38d256ec4
mimuogi <mimuogi@gmail.com>
2024-07-27 23:23:29 +0200

    Scroll to 0,0 added

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,8 +1,13 @@
 import { useLoaderData } from "react-router-dom";
 import { PostView } from "../components/post-view/post-view";
 import { PostType } from "../types/Post";
+import { useEffect } from "react";
 export function PostPage() {
 	const { post } = useLoaderData() as { post: PostType };
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [post.id]);
 
 	return (
 		<div>
