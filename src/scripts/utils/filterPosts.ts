7a361998a3302f404c99358134e7570b9bdf5255
mimuogi <mimuogi@gmail.com>
2024-09-23 15:22:01 +0200

    Updated filterPost method to sort posts

--- src/scripts/utils/filterPosts.ts
+++ src/scripts/utils/filterPosts.ts
@@ -5,9 +5,21 @@ export function filterPosts(
 	excludeTag: string,
 	includeTag = ""
 ) {
-	return posts.filter(
-		(post) =>
-			!post.tags.includes(excludeTag) &&
-			(includeTag ? post.tags.includes(includeTag) : true)
-	);
+	return posts
+		.filter(
+			(post) =>
+				!post.tags.includes(excludeTag) &&
+				(includeTag ? post.tags.includes(includeTag) : true)
+		)
+		.sort((a, b) => {
+			// Date from dd/mm/yyyy to yyyy-mm-dd for comparison
+			const [dayA, monthA, yearA] = a.date.split("/").map(Number);
+			const [dayB, monthB, yearB] = b.date.split("/").map(Number);
+
+			const dateA = new Date(yearA, monthA - 1, dayA); // JS months are 0-indexed
+			const dateB = new Date(yearB, monthB - 1, dayB);
+
+			return dateB.getTime() - dateA.getTime();
+		});
 }
+
